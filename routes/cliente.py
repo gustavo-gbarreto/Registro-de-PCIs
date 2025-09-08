fdc670313913f0df3b0408b05262d0fb628d398c
gustavo-gbarreto <gustavogomes1106@gmail.com>
2025-09-08 17:23:31 -0300

    add route documentation for cliente blueprint

--- routes/cliente.py
+++ routes/cliente.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template
 
 cliente_route = Blueprint('cliente', __name__)
 
+"""
+ -/cliente/lotes (GET) - Lista os lotes cadastrados
+ -/cliente/lotes (POST) - Cadastrar um novo lote(precisa de permissão especial)
+ -/cliente/lote_id (GET) - Detalhes do lote específico
+ -/cliente/lote_id/serial_id (GET) - detalhe de uma PCI especiífica
+ -/cliente/lote_id/serial_id/edit (PUT) - editar uma PCI específica(precisa de permissão especial)
+ -/cliente/lote_id/serial_id/delete (DELETE) - deletar uma PCI específica(precisa de permissão especial)
+"""
 @cliente_route.route('/')
 def listaPCI():
     pass
@@ -10,3 +18,4 @@ def listaPCI():
 @cliente_route.route('/<char:lote_id>')
 def obter_PCI():
     pass
+    
