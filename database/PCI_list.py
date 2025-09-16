0ecd66aee802c0a78696b479b0f8349755d60a3b
gustavo-gbarreto <gustavogomes1106@gmail.com>
2025-09-16 13:34:55 -0300

    .

--- database/PCI_list.py
+++ database/PCI_list.py
@@ -1,10 +1,10 @@
 PCI = [
-    {"#": "1", "lote id": 1, "Serial_Number": "L01-NCB-Rev4-001-0725", "Montada em:": "2025-07-28", "test. result.:": "Aprovada", 
-     "tec. teste": "Gustavo Barreto", "retrabalho": "Não", "tèc. retrabalho": "N/A", "observações": "N/A"},
-    {"#": "2", "lote id": 1, "Serial_Number": "L01-NCB-Rev4-002-0725", "Montada em:": "2025-07-28", "test. result.:": "Aprovada", 
-     "tec. teste": "Gustavo Barreto", "retrabalho": "Não", "tèc. retrabalho": "N/A", "observações": "N/A"},
-    {"#": "3", "lote id": 1, "Serial_Number": "L01-NCB-Rev4-003-0725", "Montada em:": "2025-07-28", "test. result.:": "Aprovada", 
-     "tec. teste": "Gustavo Barreto", "retrabalho": "Não", "tèc. retrabalho": "N/A", "observações": "N/A"},
-    {"#": "4", "lote id": 1, "Serial_Number": "L01-NCB-Rev4-004-0725", "Montada em:": "2025-07-28", "test. result.:": "Reprovada", 
-     "tec. teste": "Gustavo Barreto", "retrabalho": "CI soldado invertido", "tèc. retrabalho": "Gustavo Barreto", "observações": ""}
+    {"item": "1", "Lote ID": 1, "Serial Number": "L01-NCB-Rev4-001-0725", "Data de Montagem": "2025-07-28", "Resultado do Teste": "Aprovada", 
+     "técnico do teste": "Gustavo Barreto", "Retrabalho": "Não", "Técnico do Retrabalho": "N/A", "Observações": "N/A"},
+    {"item": "2", "Lote ID": 1, "Serial Number": "L01-NCB-Rev4-002-0725", "Data de Montagem": "2025-07-28", "Resultado do Teste": "Aprovada", 
+     "técnico do teste": "Gustavo Barreto", "Retrabalho": "Não", "Técnico do Retrabalho": "N/A", "Observações": "N/A"},
+    {"item": "3", "Lote ID": 1, "Serial Number": "L01-NCB-Rev4-003-0725", "Data de Montagem": "2025-07-28", "Resultado do Teste": "Aprovada", 
+     "técnico do teste": "Gustavo Barreto", "Retrabalho": "Não", "Técnico do Retrabalho": "N/A", "Observações": "N/A"},
+    {"item": "4", "Lote ID": 1, "Serial Number": "L01-NCB-Rev4-004-0725", "Data de Montagem": "2025-07-28", "Resultado do Teste": "Reprovada", 
+     "técnico do teste": "Gustavo Barreto", "Retrabalho": "CI soldado invertido", "Técnico do Retrabalho": "Gustavo Barreto", "Observações": ""}
 ]
